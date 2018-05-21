553b2bf00e71168924db6e9ea18a1e53f2abb383
ichiroc <nobitahouse@gmail.com>
2018-05-22 07:05:02 +0900

    起動時に都道府県の一覧表示

--- App.js
+++ App.js
@@ -4,12 +4,27 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+// @flow
+import { StackNavigator } from 'react-navigation';
+import CityListScreen from './src/CityListScreen';
 import WeatherScreen from './src/WeatherScreen';
 
+const routes = {
+  CityList: {
+    screen: CityListScreen,
+    navigationOptions: {
+      title: '都道府県一覧',
+    },
+  },
+  Weather: {
+    screen: WeatherScreen,
+  },
+};
 
-export default class App extends Component<{}> {
-  render() {
-    return <WeatherScreen />
-  }
-}
+const config = {
+  initialRouteName: 'CityList',
+};
+
+const App = StackNavigator(routes, config);
+
+export default App;
